3017e91a0f07e3ff21d877f8bfa9dd6ec1f88beb
hayduke <33008490+SeldomseenSchweig@users.noreply.github.com>
2024-06-17 19:10:11 +0000

    added petition

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function darkMode(){
 }
 
 btn.addEventListener( 'click', darkMode)
+
+
+const formButton = document.getElementById('sign-now-button');  
+const addSignature = (event) => {
+  const name = document.getElementById('name').value;
+  if(!name) {
+    alert('Please enter your name');
+    return}
+  const hood = document.getElementById('hood').value;
+  if(!hood) {
+    alert('Please enter your hood');
+    return}
+  const email = document.getElementById('email').value
+  if(!email) {
+    alert('Please enter your email');
+    return}
+  console.log("The name is: ",name, hood, email)
+    
+  const newSignature = document.createElement('p');
+  newSignature.innerText = `🖊️ ${name} from
+  ${hood} supports this.`;
+  const sigs = document.getElementsByClassName('signatures')[0];
+  sigs.appendChild(newSignature)
+  event.preventDefault();
+}
+formButton.addEventListener('click', addSignature);
+
+
+// Add a click event listener to the sign now button here
