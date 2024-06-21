dd7c5ebdd1fb5733284646f261280bf4b1ba13e0
hayduke <33008490+SeldomseenSchweig@users.noreply.github.com>
2024-06-21 21:06:54 +0000

    finished

--- script.js
+++ script.js
@@ -4,38 +4,41 @@ function toggleMenu() {
 }
 
 const btn = document.getElementById('toggle-theme-button');
-function darkMode(){
+function darkMode() {
   const body = document.querySelector('body');
   body.classList.toggle('dark-mode');
 }
 
-btn.addEventListener( 'click', darkMode)
+btn.addEventListener('click', darkMode)
 
 
-const formButton = document.getElementById('sign-now-button');  
+const formButton = document.getElementById('sign-now-button');
 const addSignature = (event) => {
   const name = document.getElementById('name').value;
-  if(!name) {
+  if (!name) {
     alert('Please enter your name');
-    return}
+    return
+  }
   const hood = document.getElementById('hood').value;
-  if(!hood) {
-    alert('Please enter your hood');
-    return}
+  if (!hood) {
+    alert('Please enter your hood'); g
+    return
+  }
   const email = document.getElementById('email').value
-  if(!email) {
+  if (!email) {
     alert('Please enter your email');
-    return}
-  console.log("The name is: ",name, hood, email)
-    
+    return
+  }
+
   const newSignature = document.createElement('p');
   newSignature.innerText = `🖊️ ${name} from
   ${hood} supports this.`;
   const sigs = document.getElementsByClassName('signatures')[0];
   sigs.appendChild(newSignature)
   document.getElementById("sign-petition").reset()
+  openModal(name, hood)
   event.preventDefault();
-  
+
 }
 
 
@@ -45,15 +48,14 @@ const validateForm = () => {
   const email = document.getElementById('email');
   if (!email.value.includes('.com')) {
     email.classList.add('error');
-  }else {
-      email.classList.remove('error');
+  } else {
+    email.classList.remove('error');
   }
   let containsErrors = false;
 
   var petitionInputs = document.getElementById("sign-petition").elements;
   // TODO: Loop through all inputs
-  console.log(petitionInputs)
-  for(let i = 0; i < petitionInputs.length; i++){
+  for (let i = 0; i < petitionInputs.length; i++) {
     console.log(petitionInputs[i].id)
     if (petitionInputs[i].value.length < 2) {
       console.log("error")
@@ -66,7 +68,63 @@ const validateForm = () => {
   }
 
   // TODO: Validate the value of each input
-  if (containsErrors == false)  addSignature();
+  if (containsErrors == false) addSignature();
 }
 let signNowButton = document.getElementById('sign-now-button');
 signNowButton.addEventListener('click', validateForm);
+
+let animation = {
+  revealDistance: 150,
+  initialOpacity: 0,
+  transitionDelay: 0,
+  transitionDuration: '2s',
+  transitionProperty: 'all',
+  transitionTimingFunction: 'ease'
+}
+
+
+
+
+let revealableContainers = document.getElementsByClassName('revealable');
+
+function reveal() {
+  for (let i = 0; i < revealableContainers.length; i++) {
+    let windowHeight = window.innerHeight;
+    console.log()
+    let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+    if (topOfRevealableContainer < windowHeight - 150) {
+      revealableContainers[i].classList.add('active');
+    } else {
+      revealableContainers[i].classList.remove('active');
+    }
+  }
+}
+window.addEventListener('scroll', reveal);
+
+reveal()
+
+function openModal(name, location) {
+
+  document.getElementById('thanks').innerText = ` ${name} from  ${location} supports this! Awesome!`;
+  let modal = document.getElementById("petitionModal");
+  modal.style.display = "block";
+
+  setTimeout(() => {
+    let modal = document.getElementById("petitionModal");
+    modal.style.display = "none";
+  }, 5000)
+}
+
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  let modal = document.getElementById("petitionModal");
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  let modal = document.getElementById("petitionModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
